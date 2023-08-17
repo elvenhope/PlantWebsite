0352e90a0cefb371c7ec06f2cdc7b6b9b1ae553a
Aleks <aleks.grisins@gmail.com>
2023-08-17 17:25:31 +0300

    Fixed issue with a missing div closing bracket

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -85,20 +85,21 @@
             <button>Let's go</button>
         </div>
         <div class="products"> 
+            <h2>Product list</h2>
             <div class="product">
                 <div class="productImage">
                     <img src="{{ URL::asset('images/plant1.jpg') }}" alt="">
                 </div>
                 <div class="productInfo">
-                    <h2>Grape</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Product info</p>
                 </div>
             </div>
             <div class="product">
                 <div class="productImage">
                     <img src="{{ URL::asset('images/plant2.jpg') }}" alt="">
                 </div>
-            <button>View all</button>
+                <button>View all</button>
+            </div>
         </div>
         <div class="shipping-notice">
             <img src="{{ URL::asset('images/shipping-notice.png') }}" alt="">
