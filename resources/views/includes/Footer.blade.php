fd30b420bcd075ed8ef99d8897b63f7c6e0f524b
Davit Zuroshvili <davitzuroshvili@gmail.com>
2023-08-18 14:03:48 +0300

    moved everything to the blade layout style

---
+++ resources/views/includes/Footer.blade.php
@@ -0,0 +1,40 @@
+<div class="footer-content">
+    <div class="logo">
+        <img src="assets/logo.svg" alt="Logo">
+    </div>
+    <div class="footer-links">
+        <div class="footer-column">
+            <h3>My Account</h3>
+            <a href="#">Log in</a>
+            <a href="#">Cart</a>
+        </div>
+        <div class="footer-column">
+            <h3>Shop</h3>
+            <a href="#">Plants</a>
+            <a href="#">Pots</a>
+        </div>
+        <div class="footer-column">
+            <h3>Customer Service</h3>
+            <a href="#">FAQ</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Shipping</a>
+        </div>
+        <div class="footer-column">
+            <h3>About</h3>
+            <a href="#">Blog</a>
+            <a href="#">Careers</a>
+            <a href="#">About us</a>
+        </div>
+    </div>
+    <div class="soc-media">
+        <a href="#" class="facebook">
+            <i class="fa-brands fa-facebook-f" aria-hidden="true"></i>
+        </a>
+        <a href="#" class="instagram">
+            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="#" class="twitter">
+            <i class="fa-brands fa-twitter" aria-hidden="true"></i>
+        </a>
+    </div>
+</div>
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src="https://kit.fontawesome.com/e63de0730d.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+<script src="{{ asset('js/script.js') }}"></script>
+<title>@yield("pageTitle")</title>
+@yield("PageSpecificPropertiesInTheHead")
