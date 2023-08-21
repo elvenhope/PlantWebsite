9a5adedfa8bf635ea57577c67c072cd2a982a884
Davit Zuroshvili <davitzuroshvili@gmail.com>
2023-08-21 12:23:46 +0300

    saved for editing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index() {
-
-        return view('pages.home');
+        $products = Product::select("*")->offset(0)->limit(10)->get();
+        return view('pages.home', ['products' => $products]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
 });
