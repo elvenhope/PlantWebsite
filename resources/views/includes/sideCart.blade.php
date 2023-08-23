0246d77a6f08f3f07fe986e373626d5b0bd7cadf
Braiens <b.stuglis@gmail.com>
2023-08-23 18:02:22 +0300

    side cart fix

--- resources/views/includes/sideCart.blade.php
+++ resources/views/includes/sideCart.blade.php
@@ -16,50 +16,40 @@
             <!-- Item -->
             @if (session('cart'))
                 @foreach (session('cart') as $id => $details)
-
-
-
-                        @isset($details['product_id'])
-                            <div class="cart_item" data-value="{{ $details['product_id'] }}">
+                    @isset($details['product_id'])
+                    <div class="cart_item" data-value="{{ $details['product_id'] }}">
                         @else
-                            <div class="cart_item" data-value="">
-                        @endisset
-
-
-
-                        <div class="remove_item remove-from-cart">
-                            <span>&times;</span>
-                        </div>
-                        <div class="item_img">
-                            <img src="{{ $details['imgLink'] }}" />
-                        </div>
-                        <div class="item_details">
-                            <p>{{ $details['name'] }}</p>
-                            <strong class="item_details-price">${{ $details['price'] }}</strong>
-                            <div class="qty">
-                                <span class="update-cart-minus">-</span>
-                                <strong class="update-cart-value">{{ $details['quantity'] }}</strong>
-                                <span class="update-cart-plus">+</span>
+                        <div class="cart_item" data-value="">
+                            @endisset
+                            <div class="remove_item remove-from-cart">
+                                <span>&times;</span>
                             </div>
-                        </div>
+                            <div class="item_img">
+                                <img src="{{ $details['imgLink'] }}" />
+                            </div>
+                            <div class="item_details">
+                                <p>{{ $details['name'] }}</p>
+                                <strong class="item_details-price">${{ $details['price'] }}</strong>
+                                <div class="qty">
+                                    <span class="update-cart-minus">-</span>
+                                    <strong class="update-cart-value">{{ $details['quantity'] }}</strong>
+                                    <span class="update-cart-plus">+</span>
+                                </div>
+                            </div>
                     </div>
                 @endforeach
             @endif
-            <!-- /Item -->
-            <!-- Cart Actions -->
-            <div class="cart_actions">
-                @php $total = 0 @endphp
-                @foreach ((array) session('cart') as $id => $details)
-                    @php $total += $details['price'] * $details['quantity'] @endphp
-                @endforeach
-                <div class="subtotal">
-                    <p>TOTAL:</p>
-                    <p><span id="subtotal_price">$ {{ $total }}</span></p>
-                </div>
-                <a href="{{ route('cart') }}" type="button" class="cart_btn side_cart_link">View Cart</a>
-                {{-- <button class="cart_btn"><a href="#">View Cart</a></button> --}}
-
-                <button class="cart_btn">Checkout</button>
+            
+        <!-- /Item -->
+        <!-- Cart Actions -->
+        <div class="cart_actions">
+            @php $total = 0 @endphp
+            @foreach ((array) session('cart') as $id => $details)
+            @php $total += $details['price'] * $details['quantity'] @endphp
+            @endforeach
+            <div class="subtotal">
+                <p>TOTAL:</p>
+                <p><span id="subtotal_price">$ {{ $total }}</span></p>
             </div>
             <a href="/cart">
                 <button class="cart_btn btn1">View Cart</button>
@@ -69,90 +59,91 @@
             </a>
         </div>
     </div>
-    @section('scripts')
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script>
-            // Side Cart
-            const openBtn = document.getElementById('open_cart_btn');
-            const cart = document.getElementById('sidecart');
-            const closeBtn = document.getElementById('close_btn');
-            const backdrop = document.querySelector('.backdrop');
+</div>
+@section('scripts')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script>
+    // Side Cart
+    const openBtn = document.getElementById('open_cart_btn');
+    const cart = document.getElementById('sidecart');
+    const closeBtn = document.getElementById('close_btn');
+    const backdrop = document.querySelector('.backdrop');
 
-            openBtn.addEventListener('click', openCart)
-            closeBtn.addEventListener('click', closeCart)
-            backdrop.addEventListener('click', closeCart)
+    openBtn.addEventListener('click', openCart)
+    closeBtn.addEventListener('click', closeCart)
+    backdrop.addEventListener('click', closeCart)
 
-            // Open Cart
-            function openCart() {
-                cart.classList.add('open');
-                backdrop.style.display = 'block';
+    // Open Cart
+    function openCart() {
+        cart.classList.add('open');
+        backdrop.style.display = 'block';
 
-                setTimeout(() => {
-                    backdrop.classList.add('show');
-                }, 0);
-            }
+        setTimeout(() => {
+            backdrop.classList.add('show');
+        }, 0);
+    }
 
-            // Close Cart
-            function closeCart() {
-                cart.classList.remove('open');
-                backdrop.classList.remove('show');
+    // Close Cart
+    function closeCart() {
+        cart.classList.remove('open');
+        backdrop.classList.remove('show');
 
-                setTimeout(() => {
-                    backdrop.style.display = 'none';
-                }, 500);
-            }
+        setTimeout(() => {
+            backdrop.style.display = 'none';
+        }, 500);
+    }
 
-            // $(".update-cart-minus").click(function (e) {
-            //     e.preventDefault();
+    // $(".update-cart-minus").click(function (e) {
+    //     e.preventDefault();
 
-            //     var btn_minus = document.querySelector('.update-cart-value').innerHTML
-            //     console.log(btn_minus);
-            //     // console.log(ele)
-            //     $.ajax({
-            //         url: "{{ route('update.cart') }}",
-            //         method: "patch",
-            //         data: {
-            //             _token: '{{ csrf_token() }}',
-            //             id: ele.parents("tr").attr("data-id"),
-            //             quantity: ele.parents("tr").find(".quantity").val()
-            //         },
-            //         success: function (response) {
-            //             // window.location.reload();
-            //         }
-            //     });
-            // });
+    //     var btn_minus = document.querySelector('.update-cart-value').innerHTML
+    //     console.log(btn_minus);
+    //     // console.log(ele)
+    //     $.ajax({
+    //         url: "{{ route('update.cart') }}",
+    //         method: "patch",
+    //         data: {
+    //             _token: '{{ csrf_token() }}',
+    //             id: ele.parents("tr").attr("data-id"),
+    //             quantity: ele.parents("tr").find(".quantity").val()
+    //         },
+    //         success: function (response) {
+    //             // window.location.reload();
+    //         }
+    //     });
+    // });
 
-            // $(".update-cart-plus").click(function (e) {
-            //     e.preventDefault();
+    // $(".update-cart-plus").click(function (e) {
+    //     e.preventDefault();
 
-            //     var ele = document.querySelector('.update-cart')
-            //     // console.log(ele)
-            //     $.ajax({
-            //         url: "{{ route('update.cart') }}",
-            //         method: "patch",
-            //         data: {
-            //             _token: '{{ csrf_token() }}',
-            //             id: ele.parents("tr").attr("data-id"),
-            //             quantity: ele.parents("tr").find(".quantity").val()
-            //         },
-            //         success: function (response) {
-            //             // window.location.reload();
-            //         }
-            //     });
-            // });
+    //     var ele = document.querySelector('.update-cart')
+    //     // console.log(ele)
+    //     $.ajax({
+    //         url: "{{ route('update.cart') }}",
+    //         method: "patch",
+    //         data: {
+    //             _token: '{{ csrf_token() }}',
+    //             id: ele.parents("tr").attr("data-id"),
+    //             quantity: ele.parents("tr").find(".quantity").val()
+    //         },
+    //         success: function (response) {
+    //             // window.location.reload();
+    //         }
+    //     });
+    // });
 
-    function setHeaders(headers){
-        for(let key in headers){
+    function setHeaders(headers) {
+        for (let key in headers) {
             xhr.setRequestHeader(key, headers[key])
         }
     }
 
-    document.addEventListener("DOMContentLoaded", function () {
-    const removeButtons = document.querySelectorAll(".remove-from-cart");
+    document.addEventListener("DOMContentLoaded", function() {
+        const removeButtons = document.querySelectorAll(".remove-from-cart");
 
         removeButtons.forEach(button => {
-            button.addEventListener("click", function (e) {
+            button.addEventListener("click", function(e) {
                 e.preventDefault();
 
                 if (confirm("Are you sure you want to delete?")) {
@@ -163,7 +154,7 @@
                     const xhr = new XMLHttpRequest();
                     xhr.open("DELETE", `{{ route('remove.from.cart') }}?product_id=${productId}`);
                     xhr.setRequestHeader("X-CSRF-TOKEN", '{{ csrf_token() }}');
-                    xhr.onload = function () {
+                    xhr.onload = function() {
                         if (xhr.status === 200) {
                             cartItem.remove();
                             updateCartTotal();
@@ -171,7 +162,7 @@
                             console.error("Error:", xhr.status, xhr.statusText);
                         }
                     };
-                    xhr.onerror = function () {
+                    xhr.onerror = function() {
                         console.error("Network error");
                     };
                     xhr.send();
@@ -180,7 +171,7 @@
         });
     });
 
-    $(".update-cart-minus").click(function (e) {
+    $(".update-cart-minus").click(function(e) {
         e.preventDefault();
 
         var btn_minus = $(this).siblings('.update-cart-value');
@@ -192,7 +183,7 @@
         }
     });
 
-    $(".update-cart-plus").click(function (e) {
+    $(".update-cart-plus").click(function(e) {
         e.preventDefault();
 
         var btn_plus = $(this).siblings('.update-cart-value');
@@ -214,13 +205,13 @@
                 id: productId,
                 quantity: quantity
             },
-            success: function (response) {
+            success: function(response) {
                 if (response.success) {
                     cartItem.find('.update-cart-value').text(quantity);
                     //updateCartTotal();
                 }
             },
-            error: function (xhr) {
+            error: function(xhr) {
                 console.error("Error:", xhr.status, xhr.statusText);
             }
         });
